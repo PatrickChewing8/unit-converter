bd13086c620474e5a5e369afb4f08598aeea8a31
PatrickChewing8 <84030379+PatrickChewing8@users.noreply.github.com>
2022-12-03 16:58:28 -0500

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let convertBtn = document.getElementById('convert-btn');
 let inputEl = document.getElementById('input');
 let lengthEl = document.getElementById('length-el');
+let volumeEl = document.getElementById('volume-el');
+let massEl = document.getElementById('mass-el');
 
 const meterToFeet = 3.281;
 const literToGallon = 0.264;
@@ -9,5 +11,13 @@ const kiloToPound = 2.204;
 convertBtn.addEventListener('click', function () {
 	let baseValue = inputEl.value;
 
-	lengthEl.textContent = `${baseValue} meter = ${baseValue * meterToFeet} feet`;
+	lengthEl.textContent = `${baseValue} meters = ${
+		(baseValue * meterToFeet).toFixed(3)
+	} feet`;
+	volumeEl.textContent = `${baseValue} liters = ${
+		(baseValue * literToGallon).toFixed(3)
+	} gallons`;
+	massEl.textContent = `${baseValue}  kilos = ${
+		(baseValue * kiloToPound).toFixed(3)
+	} pounds`;
 });
